71222ca10342fd8bd433c5826b780a3172a62ef4
69070026-rgb <69070026@kmitl.ac.th>
2026-08-16 23:51:41 +0700

    night before exam

---
+++ Practice/P2996.py
@@ -0,0 +1,8 @@
+"""Swap characters"""
+
+def main():
+    """Swap characters from front to back"""
+    character = input()
+    swap = character[::-1].lower()
+    print(swap)
+main()
---
+++ Practice/test1.py
@@ -0,0 +1,10 @@
+""""""
+
+def main():
+    """"""
+    num =int(input())
+    factorial = 1
+    for i in range(1 ,num + 1):
+        factorial *= i
+    print(factorial)
+main()
